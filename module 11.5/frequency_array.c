8d9d1203b1f616efb84ec4db9fa39635250d50ec
sholayman99 <mdsholayman998@gmail.com>
2025-04-25 15:27:38 +0600

    module 11.5 frequency array completed

---
+++ module 11.5/frequency_array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int n ;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0 ; i<n ; i++){
+       scanf("%d",&a[i]);
+    }
+
+    int fre[6]={0};
+
+    for (int i=0 ; i<n ; i++){
+        int val = a[i];
+        fre[a[i]]++ ;
+    }
+    for( int i=0 ; i<6 ; i++){
+        printf("%d -> %d\n",i,fre[i]);
+    }
+    return 0;
+}
